7779175301d47236663f3ca891cbe1437ad0c3c1
#Dm <108529559+DmitryDemidov48@users.noreply.github.com>
2024-02-10 23:27:37 +0300

    0.13

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,43 +5,57 @@ import { Link } from 'react-router-dom';
 const HeaderContainer = styled.div`
   display: flex;
   align-items: flex-end;
-  justify-content: space-between; /* Равномерное распределение элементов по горизонтали */
-  background-color: #000; /* Черный фон */
-  color: #fff; /* Белый текст */
-  padding: 20px; /* Внутренние отступы */
+  justify-content: space-between;
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+
+  /* Медиа-запрос для планшетов */
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  /* Медиа-запрос для мобильных устройств */
+  @media screen and (max-width: 480px) {
+    padding: 10px;
+  }
 `;
 
-// Создаем стилизованный компонент для названия сайта
 const SiteName = styled.h2`
-  margin: 0; /* Убираем внешние отступы */
+  margin: 0;
 `;
 
-// Создаем стилизованный компонент для кнопки ссылки
 const ButtonLink = styled(Link)`
   display: inline-block;
   padding: 10px 20px;
-  background-color: transparent; /* Прозрачный фон */
+  background-color: transparent;
   color: #fff;
   text-decoration: none;
   border-radius: 5px;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #333; /* Темно-серый фон при наведении */
+    background-color: #333;
+  }
+
+  /* Медиа-запрос для мобильных устройств */
+  @media screen and (max-width: 480px) {
+    padding: 8px 16px;
   }
 `;
 
 const Header = () => {
     return (
         <>
-        <HeaderContainer>
-            <SiteName>Shorten Link Service</SiteName>
-            <div>
-                <ButtonLink to="/linkshort">Home</ButtonLink>
-                <ButtonLink to="/login">Login</ButtonLink>
-                <ButtonLink to="/register">Register</ButtonLink>
-            </div>
-        </HeaderContainer>
+            <HeaderContainer>
+                <SiteName>Shorten Link Service</SiteName>
+                <div>
+                    <ButtonLink to="/linkshort">Home</ButtonLink>
+                    <ButtonLink to="/login">Login</ButtonLink>
+                    <ButtonLink to="/register">Register</ButtonLink>
+                </div>
+            </HeaderContainer>
         </>
     );
 };
